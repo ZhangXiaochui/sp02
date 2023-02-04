8a5cc531b5f2a6609f65c2e49652707f72f7eb95
ZhangXiaochui <2191333533@qq.com>
2023-02-04 09:23:16 +0800

    aaa

---
+++ src/main/java/com/example/sp02/controller/Articlescontroller.java
@@ -0,0 +1,14 @@
+package com.example.sp02.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author 张小锤
+ * @site www.xiaomage.com
+ * @company xxx公司
+ * @create 2023-02-04 8:59
+ */
+@Controller
+public class Articlescontroller {
+
+}
--- src/main/java/com/example/sp02/model/Articles.java
+++ src/main/java/com/example/sp02/model/Articles.java
@@ -1,5 +1,8 @@
 package com.example.sp02.model;
 
+import lombok.Data;
+
+@Data
 public class Articles {
     private Integer id;
 
@@ -7,27 +10,4 @@ public class Articles {
 
     private String body;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title == null ? null : title.trim();
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body == null ? null : body.trim();
-    }
 }
